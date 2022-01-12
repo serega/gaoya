6c8be9e7516e7f725dc060eec0d3523e12b5528d
sergey <sergey.melderis@gmail.com>
2022-01-11 23:41:07 -0500

    another attempt

--- py-gaoya/tests/test_minhash.py
+++ py-gaoya/tests/test_minhash.py
@@ -13,12 +13,12 @@ def test_minhash_64bit():
 
 
 def test_minhash_32bit():
-    index = gaoya.minhash.MinHash32StringIntIndex(0.5, 20, 5)
-    index.insert_document(1, "a b c d e")
+    index = gaoya.minhash.MinHash32StringIntIndex(0.5, 30, 5)
+    index.insert_document(1, "a b c d e f")
     index.insert_document(2, "1 2 3 4 5 6 8")
 
-    assert index.query_tokens(['a', 'b', 'c', 'd', 'e']) == [1]
-    assert index.query_tokens(['a', 'b', 'c', 'd', 'f']) == [1]
+    assert index.query_tokens(['a', 'b', 'c', 'd', 'e', 'f']) == [1]
+    assert index.query_tokens(['a', 'b', 'c', 'd', 'e', 'g']) == [1]
     assert index.query_tokens(['a', 'b', 'h', 'q', 'f']) == []
 
     assert index.query("1 2 3 4 5 6 8") == [2]
@@ -26,7 +26,7 @@ def test_minhash_32bit():
     assert index.query_tokens(['9', '2', '3', '4', '5', '6', '7', '8']) == [2]
 
 def test_minhash_16bit():
-    index = gaoya.minhash.MinHash16StringIntIndex(0.5, 20, 5)
+    index = gaoya.minhash.MinHash16StringIntIndex(0.5, 40, 5)
     index.insert_tokens(1, ['a', 'b', 'c', 'd', 'e'])
 
     assert index.query_tokens(['a', 'b', 'c', 'd', 'e']) == [1]
