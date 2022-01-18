effae3e19b997a5a71bac350d6cb6ef76ac69cd3
sergey <sergey.melderis@gmail.com>
2022-01-17 23:03:52 -0500

    skip source for now

--- .github/deploy_manylinux.sh
+++ .github/deploy_manylinux.sh
@@ -5,4 +5,5 @@ rustup override set stable
 export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+popcnt'
 maturin publish \
   -r https://test.pypi.org/legacy/ \
+  --no-sdist \
   --username serega
